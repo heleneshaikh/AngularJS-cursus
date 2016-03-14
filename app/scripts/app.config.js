7df4b211f7ec92c061935f380c37b48057e47c15
heleneshaikh <helene.shaikh@gmail.com>
2016-03-14 15:32:29 +0100

    add config

---
+++ app/scripts/app.config.js
@@ -0,0 +1,14 @@
+(function() {
+	angular.module('demoApp.config', []) // Will be added as a dependency for app module
+		   .constant('appVersion', '0.0.2');
+
+
+
+
+
+	
+
+
+
+
+})();
---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+(function() { //always use immediate functions 
+	//everything gets done in separate modules. Normally not much code in main module
+
+	angular.module('demoApp', ['demoApp.config']) //add as a dependency, module from app.config.js . Dependencies run first.
+		.config(function(appVersion) { //config: at startup. Config always happens first
+			console.log('config demoApp ' + appVersion);
+		})
+		.run(function() {
+			console.log('run demoApp');
+		});
+
+
+
+
+})();
