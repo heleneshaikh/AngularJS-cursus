0bf8c16801e1caf00702d575c66c31b61f15bf0d
heleneshaikh <helene.shaikh@gmail.com>
2016-03-15 12:01:52 +0100

    add views

--- app/scripts/app.core.js
+++ app/scripts/app.core.js
@@ -1,6 +1,19 @@
 'use strict';
 
 (function() {
-  angular.module('demoApp.core', ['ngRoute']);
+  angular.module('demoApp.core', ['ngRoute'])
+         .config(function($routeProvider) {
+           $routeProvider.when('/home', {
+             templateUrl: 'views/home.html'
+           });
+           $routeProvider.when('/form', {
+              templateUrl: 'views/form.html'
+           });
+           $routeProvider.otherwise( {
+             redirectTo: '/home'
+              //or templateUrl: ''
+           });
+
+         });
 
 })();
